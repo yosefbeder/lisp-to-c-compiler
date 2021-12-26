07a8e2f8646c92613cc2afda8beee13f2b382044
yosefbeder <dryosefbeder@gmail.com>
2021-12-26 07:33:58 +0200

    feat: add traversal

--- src/index.js
+++ src/index.js
@@ -178,4 +178,47 @@ const parser = tokens => {
 	return ast;
 };
 
-module.exports = { tokenizer, parser };
+const traverser = (ast, visitor) => {
+	const traverseNode = (node, parent = null) => {
+		if (node.type === 'Program') {
+			visitor.Program.enter(node, parent);
+
+			let current = 0;
+
+			while (current < node.body.length) {
+				let childNode = node.body[current];
+
+				traverseNode(childNode, node);
+				current++;
+				continue;
+			}
+
+			visitor.Program.exit(node, parent);
+		}
+
+		if (node.type === 'CallExpression') {
+			visitor.CallExpression.enter(node, parent);
+
+			let current = 0;
+
+			while (current < node.params.length) {
+				let childNode = node.params[current];
+
+				traverseNode(childNode, node);
+				current++;
+				continue;
+			}
+
+			visitor.CallExpression.exit(node, parent);
+		}
+
+		if (node.type === 'StringLiteral' || node.type === 'NumberLiteral') {
+			visitor[node.type].enter(node, parent);
+			visitor[node.type].exit(node, parent);
+		}
+	};
+
+	traverseNode(ast);
+};
+
+module.exports = { tokenizer, parser, traverser };
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-const { tokenizer, parser } = require('.');
+const { tokenizer, parser, traverser } = require('.');
 
 describe('tokenizer', () => {
 	it('should push parenteses', () => {
@@ -95,5 +95,117 @@ describe('parser', () => {
 	it('should throw an error when something is wrong', () => {
 		// 🤷‍♂️
 		expect(1).toBe(1);
+	});
+});
+
+describe('traverser', () => {
+	const ast = {
+		type: 'Program',
+		body: [
+			{
+				type: 'CallExpression',
+				name: 'add',
+				params: [
+					{
+						type: 'NumberLiteral',
+						value: 12,
+					},
+					{
+						type: 'StringLiteral',
+						value: 'Yosef',
+					},
+				],
+			},
+		],
+	};
+
+	it('should apply the methods in the visitor for each node in two times', () => {
+		let counter = 0;
+
+		const visitor = {
+			Program: {
+				enter() {
+					counter++;
+				},
+				exit() {
+					counter++;
+				},
+			},
+			CallExpression: {
+				enter() {
+					counter++;
+				},
+				exit() {
+					counter++;
+				},
+			},
+			NumberLiteral: {
+				enter() {
+					counter++;
+				},
+				exit() {
+					counter++;
+				},
+			},
+			StringLiteral: {
+				enter() {
+					counter++;
+				},
+				exit() {
+					counter++;
+				},
+			},
+		};
+
+		traverser(ast, visitor);
+
+		expect(counter).toBe(8);
+	});
+
+	it('should pass the parent and the node in each node traversal', () => {
+		const visitor = {
+			Program: {
+				enter(node, parent) {
+					expect(node).toEqual(ast);
+					expect(parent).toBeNull();
+				},
+				exit(node, parent) {
+					expect(node).toEqual(ast);
+					expect(parent).toBeNull();
+				},
+			},
+			CallExpression: {
+				enter(node, parent) {
+					expect(parent).toEqual(ast);
+					expect(node).toEqual(ast.body[0]);
+				},
+				exit(node, parent) {
+					expect(parent).toEqual(ast);
+					expect(node).toEqual(ast.body[0]);
+				},
+			},
+			NumberLiteral: {
+				enter(node, parent) {
+					expect(parent).toEqual(ast.body[0]);
+					expect(node).toEqual(ast.body[0].params[0]);
+				},
+				exit(node, parent) {
+					expect(parent).toEqual(ast.body[0]);
+					expect(node).toEqual(ast.body[0].params[0]);
+				},
+			},
+			StringLiteral: {
+				enter(node, parent) {
+					expect(parent).toEqual(ast.body[0]);
+					expect(node).toEqual(ast.body[0].params[1]);
+				},
+				exit(node, parent) {
+					expect(parent).toEqual(ast.body[0]);
+					expect(node).toEqual(ast.body[0].params[1]);
+				},
+			},
+		};
+
+		traverser(ast, visitor);
 	});
 });
