88e3e2d8404d8045570751fd372fb24c390e2774
yosefbeder <dryosefbeder@gmail.com>
2021-12-28 08:03:26 +0200

    feat: add compiler

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,5 +1,13 @@
 const compiler = require('..');
 
 test('compiles from lisp syntax to c-like syntax', () => {
-	expect(compiler()).toBeUndefined();
+	const input = `
+    (add 2 (subtract 4 2))
+    4
+    "yosef"
+  `;
+
+	const output = 'add(2, subtract(4, 2));\n4;\n"yosef";';
+
+	expect(compiler(input)).toBe(output);
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-const compiler = () => {};
+const {
+	tokenizer,
+	parser,
+	transformer,
+	codeGenerator,
+} = require('./components');
+
+const compiler = input => {
+	const tokens = tokenizer(input);
+	const ast = parser(tokens);
+	const newAst = transformer(ast);
+
+	return codeGenerator(newAst);
+};
 
 module.exports = compiler;
