aa8e572fe3962da5ccaf9c8c5f8eb0c01cab4733
yosefbeder <dryosefbeder@gmail.com>
2021-12-28 07:02:52 +0200

    feat: add transformer

--- src/index.js
+++ src/index.js
@@ -221,6 +221,57 @@ const traverser = (ast, visitor) => {
 	traverseNode(ast);
 };
 
-const transformer = ast => {};
+const transformNode = node => {
+	if (node.type === 'NumberLiteral' || node.type === 'StringLiteral') {
+		return node;
+	}
 
-module.exports = { tokenizer, parser, traverser };
+	return {
+		type: 'CallExpression',
+		callee: {
+			type: 'Identifier',
+			name: node.name,
+		},
+		arguments: node.params.map(node => transformNode(node)),
+	};
+};
+
+const transformer = ast => {
+	const newAst = {
+		type: 'Program',
+		body: [],
+	};
+
+	traverser(ast, {
+		NumberLiteral: {
+			enter(node, parent) {
+				if (parent.type === 'Program') {
+					newAst.body.push({ type: 'ExpressionStatement', expression: node });
+				}
+			},
+		},
+		StringLiteral: {
+			enter(node, parent) {
+				if (parent.type === 'Program') {
+					newAst.body.push({ type: 'ExpressionStatement', expression: node });
+				}
+			},
+		},
+		CallExpression: {
+			enter(node, parent) {
+				const newNode = transformNode(node);
+
+				if (parent.type === 'Program') {
+					newAst.body.push({
+						type: 'ExpressionStatement',
+						expression: newNode,
+					});
+				}
+			},
+		},
+	});
+
+	return newAst;
+};
+
+module.exports = { tokenizer, parser, traverser, transformer };
