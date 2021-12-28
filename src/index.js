e7217d18d42f3d71ed55e967e55be9ad70a56a55
yosefbeder <dryosefbeder@gmail.com>
2021-12-28 07:33:16 +0200

    feat: add codeGenerator

--- src/index.js
+++ src/index.js
@@ -274,4 +274,28 @@ const transformer = ast => {
 	return newAst;
 };
 
-module.exports = { tokenizer, parser, traverser, transformer };
+const codeGenerator = node => {
+	if (node.type === 'NumberLiteral') {
+		return String(node.value);
+	}
+
+	if (node.type === 'StringLiteral') {
+		return `"${node.value}"`;
+	}
+
+	if (node.type === 'CallExpression') {
+		return `${node.callee.name}(${node.arguments
+			.map(node => codeGenerator(node))
+			.join(', ')})`;
+	}
+
+	if (node.type === 'ExpressionStatement') {
+		return `${codeGenerator(node.expression)};`;
+	}
+
+	if (node.type === 'Program') {
+		return node.body.map(node => codeGenerator(node)).join('\n');
+	}
+};
+
+module.exports = { tokenizer, parser, traverser, transformer, codeGenerator };
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,10 @@
-const { tokenizer, parser, traverser, transformer } = require('.');
+const {
+	tokenizer,
+	parser,
+	traverser,
+	transformer,
+	codeGenerator,
+} = require('.');
 
 describe('tokenizer', () => {
 	it('should push parenteses', () => {
@@ -288,7 +294,7 @@ describe('transformer', () => {
 						params: [
 							{
 								type: 'NumberLiteral',
-								value: '2',
+								value: 2,
 							},
 							{
 								type: 'CallExpression',
@@ -296,11 +302,11 @@ describe('transformer', () => {
 								params: [
 									{
 										type: 'NumberLiteral',
-										value: '4',
+										value: 4,
 									},
 									{
 										type: 'NumberLiteral',
-										value: '2',
+										value: 2,
 									},
 								],
 							},
@@ -322,7 +328,7 @@ describe('transformer', () => {
 						arguments: [
 							{
 								type: 'NumberLiteral',
-								value: '2',
+								value: 2,
 							},
 							{
 								type: 'CallExpression',
@@ -333,11 +339,11 @@ describe('transformer', () => {
 								arguments: [
 									{
 										type: 'NumberLiteral',
-										value: '4',
+										value: 4,
 									},
 									{
 										type: 'NumberLiteral',
-										value: '2',
+										value: 2,
 									},
 								],
 							},
@@ -346,5 +352,175 @@ describe('transformer', () => {
 				},
 			],
 		});
+	});
+});
+
+describe('codeGenerator', () => {
+	it('should turn individual nodes', () => {
+		expect(
+			codeGenerator({
+				type: 'NumberLiteral',
+				value: 2,
+			}),
+		).toBe('2');
+		expect(
+			codeGenerator({
+				type: 'StringLiteral',
+				value: 'yosef',
+			}),
+		).toBe('"yosef"');
+	});
+
+	it('should turn string and number nodes inside expression statement', () => {
+		expect(
+			codeGenerator({
+				type: 'ExpressionStatement',
+				expression: {
+					type: 'NumberLiteral',
+					value: 2,
+				},
+			}),
+		).toBe('2;');
+		expect(
+			codeGenerator({
+				type: 'ExpressionStatement',
+				expression: {
+					type: 'StringLiteral',
+					value: 'yosef',
+				},
+			}),
+		).toBe('"yosef";');
+	});
+
+	it('should turn function nodes (single argument)', () => {
+		expect(
+			codeGenerator({
+				type: 'CallExpression',
+				callee: {
+					type: 'Identifier',
+					name: 'add',
+				},
+				arguments: [
+					{
+						type: 'NumberLiteral',
+						value: 2,
+					},
+				],
+			}),
+		).toBe('add(2)');
+	});
+
+	it('should turn function nodes (multiple argument)', () => {
+		expect(
+			codeGenerator({
+				type: 'CallExpression',
+				callee: {
+					type: 'Identifier',
+					name: 'add',
+				},
+				arguments: [
+					{
+						type: 'NumberLiteral',
+						value: 2,
+					},
+					{
+						type: 'NumberLiteral',
+						value: 5,
+					},
+				],
+			}),
+		).toBe('add(2, 5)');
+	});
+
+	it('should turn function nodes recursively', () => {
+		expect(
+			codeGenerator({
+				type: 'CallExpression',
+				callee: {
+					type: 'Identifier',
+					name: 'add',
+				},
+				arguments: [
+					{
+						type: 'NumberLiteral',
+						value: 2,
+					},
+					{
+						type: 'CallExpression',
+						callee: {
+							type: 'Identifier',
+							name: 'subtract',
+						},
+						arguments: [
+							{
+								type: 'NumberLiteral',
+								value: 4,
+							},
+							{
+								type: 'NumberLiteral',
+								value: 2,
+							},
+						],
+					},
+				],
+			}),
+		).toBe('add(2, subtract(4, 2))');
+	});
+
+	it('should transform a whole abstract syntax tree with more than one expression statement', () => {
+		expect(
+			codeGenerator({
+				type: 'Program',
+				body: [
+					{
+						type: 'ExpressionStatement',
+						expression: {
+							type: 'CallExpression',
+							callee: {
+								type: 'Identifier',
+								name: 'add',
+							},
+							arguments: [
+								{
+									type: 'NumberLiteral',
+									value: 2,
+								},
+								{
+									type: 'CallExpression',
+									callee: {
+										type: 'Identifier',
+										name: 'subtract',
+									},
+									arguments: [
+										{
+											type: 'NumberLiteral',
+											value: 4,
+										},
+										{
+											type: 'NumberLiteral',
+											value: 2,
+										},
+									],
+								},
+							],
+						},
+					},
+					{
+						type: 'ExpressionStatement',
+						expression: {
+							type: 'NumberLiteral',
+							value: 2,
+						},
+					},
+					{
+						type: 'ExpressionStatement',
+						expression: {
+							type: 'StringLiteral',
+							value: 'yosef',
+						},
+					},
+				],
+			}),
+		).toBe('add(2, subtract(4, 2));\n2;\n"yosef";');
 	});
 });
